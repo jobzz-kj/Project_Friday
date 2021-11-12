666ae797732381544414e64ee440bdc73736fb51
JOBLIN-K. JAMES <31209860+jobzz-kj@users.noreply.github.com>
2021-11-12 15:09:04 +0530

    Update gigs.route.js

--- routes/gigs.route.js
+++ routes/gigs.route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gigController = require('../controller/gig.controller');
 
-router.post('/',gigController.addGig);
-router.get('/',gigController.findGigs);
-router.get('/:id',gigController.findGigById);
+router.post('/',gigController.addGig);              // This means 'gigs/'
+router.get('/',gigController.findGigs);             //this means 'gigs/'
+router.get('/:id',gigController.findGigById);       //means 'gigs/id'
 router.put('/:id',gigController.updateGig);
 router.delete('/:id',gigController.deleteById);
 
