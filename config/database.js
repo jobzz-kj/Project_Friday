c4e748db19bdd8e819f40c62ceac7d8603c45141
JOBLIN-K. JAMES <31209860+jobzz-kj@users.noreply.github.com>
2021-11-12 16:12:52 +0530

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+//creating db with name codegig and uname as sqlite
+//and pass as Welcome123
+const db = new Sequelize('codegig', 'sqlite','Welcome123',{
+    host:'localhost',
+    dialect: 'sqlite',
+//pool is the way you deal with db,defining some properties like that
+
+    pool:{
+        max:5, //max no of threads
+        min:0, //min no of threads
+        acquire:30000,  //acquire the connection in 3ms
+        idle: 10000   //connection can be idle for 1ms
+    }
+});
+
+module.exports = db;
